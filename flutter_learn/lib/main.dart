e81e20e7031c2375c1b831d07568378aca140ab4
AM1T-44 <u2204044@student.cuet.ac.bd>
2024-12-10 15:33:05 +0600

    ListView

--- flutter_learn/lib/main.dart
+++ flutter_learn/lib/main.dart
@@ -40,82 +40,46 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  child: SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: [
-                        Container(
-                          height: 200,
-                          width: 200,
-                          color: Colors.grey,
-                          margin: const EdgeInsets.only(right: 10, bottom: 10),
-                        ),
-                        Container(
-                          height: 200,
-                          width: 200,
-                          color: const Color.fromARGB(148, 254, 137, 27),
-                          margin: const EdgeInsets.only(right: 10, bottom: 10),
-                        ),
-                        Container(
-                          height: 200,
-                          width: 200,
-                          color: const Color.fromARGB(255, 221, 38, 218),
-                          margin: const EdgeInsets.only(right: 10, bottom: 10),
-                        ),
-                        Container(
-                          height: 200,
-                          width: 200,
-                          color: const Color.fromARGB(255, 66, 10, 10),
-                          margin: const EdgeInsets.only(bottom: 10),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 200,
-                  color: Colors.green,
-                  margin: const EdgeInsets.only(bottom: 10),
-                ),
-                Container(
-                  height: 200,
-                  color: Colors.purple,
-                  margin: const EdgeInsets.only(bottom: 10),
-                ),
-                Container(
-                  height: 200,
-                  color: Colors.yellow,
-                  margin: const EdgeInsets.only(bottom: 10),
-                ),
-                Container(
-                  height: 200,
-                  color: Colors.red,
-                  margin: const EdgeInsets.only(bottom: 10),
-                ),
-                Container(
-                  height: 200,
-                  color: Colors.green,
-                  margin: const EdgeInsets.only(bottom: 10),
-                ),
-                Container(
-                  height: 200,
-                  color: Colors.purple,
-                  margin: const EdgeInsets.only(bottom: 10),
-                ),
-                Container(
-                  height: 200,
-                  color: Colors.yellow,
-                  margin: const EdgeInsets.only(bottom: 10),
-                ),
-              ],
+        body: ListView(
+          scrollDirection: Axis.horizontal,
+          //reverse: true,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "One",
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+              ),
             ),
-          ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Two",
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Three",
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Four",
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Five",
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),
+              ),
+            )
+          ],
         ));
   }
 }
